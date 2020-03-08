931b92bae9e2d7a77d03690637d557c6187d4b26
pms96 <ppaabloms96@gmail.com>
2020-03-08 23:24:53 +0100

    prueba

---
+++ operar_arrays/OperarArrays.java
@@ -0,0 +1,18 @@
+package operar_arrays;
+
+public class OperarArrays {
+
+	public static void main(String[] args) {
+		
+		int arrayNumeros [] = new int [10]; 
+		
+		for (int i = 0; i < arrayNumeros.length; i++) {
+			arrayNumeros [i] = (int) Math.round(Math.random()* (20 - 10)+ 1 );
+			// System.out.println(arrayNumeros [i]);
+			System.out.println("esto es una prueba");
+			
+		}
+
+	}
+
+}
